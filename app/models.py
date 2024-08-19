baf6e00929fe5ee9556acd51a67448e33316c8b2
elenaglu <shishalovae@mail.ru>
2024-08-19 20:01:29 +0500

    correct the annotation

--- app/models.py
+++ app/models.py
@@ -1,7 +1,6 @@
 from sqlalchemy import ForeignKey, text, Text
 from sqlalchemy.orm import relationship, Mapped, mapped_column
-from app.database import Base, str_uniq, int_pk, str_null_true
-from datetime import date
+from app.database import Base, str_uniq, int_pk
 
 
 class User(Base):
@@ -10,59 +9,23 @@ class User(Base):
     hashed_password: Mapped[str]
     admin: Mapped[bool]
 
-    major_id: Mapped[int] = mapped_column(ForeignKey("majors.id"), nullable=False)
-
-    major: Mapped["Major"] = relationship("Major", back_populates="users")
-
-    def __str__(self):
-        return (f"{self.__class__.__name__}(id={self.id}, "
-                f"first_name={self.first_name!r},"
-                f"last_name={self.last_name!r})")
-
-    def __repr__(self):
-        return str(self)
+    accounts: Mapped["Account"] = relationship("Account", back_populates="users")
 
 
 class Account(Base):
     id: Mapped[int_pk]
-    balance: Mapped[str_uniq]
-    major_description: Mapped[str_null_true]
-    count_students: Mapped[int] = mapped_column(server_default=text('0'))
+    balance: Mapped[decimal(18, 2)]
+    user_id = Mapped[int] = mapped_column(ForeignKey("users.id"))
 
-    def __str__(self):
-        return f"{self.__class__.__name__}(id={self.id}, major_name={self.major_name!r})"
-
-    def __repr__(self):
-        return str(self)
-
-# class User(Base):
-#     __tablename__ = "users"
-#
-#     id = Column(Integer, primary_key=True)
-#     email = Column(String, unique=True)
-#     hashed_password = Column(String)
-#     admin = Column(Boolean, default=False)
-#
-#     accounts = relationship("Account", back_populates="users")
-#
-#
-# class Account(Base):
-#     __tablename__ = "accounts"
-#
-#     id = Column(Integer, primary_key=True)
-#     balance = Column(Numeric(18, 2))
-#     user_id = Column(Integer, ForeignKey("users.id"))
-#
-#     users = relationship("User", back_populates="accounts")
-#     payments = relationship("Pay", back_populates="accounts")
+    users = Mapped["User"] = relationship("User", back_populates="accounts")
+    payments = Mapped["Pay"] = relationship("Pay", back_populates="accounts")
 
 
-# class Pay(Base):
-#     __tablename__ = "payments"
-#
-#     transaction_id = Column(String, primary_key=True)
-#     amount = Column(Numeric(18, 2))
-#     signature = Column(String)
-#     account_id = Column(Integer, ForeignKey("accounts.id"))
-#
-#     accounts = relationship("Account", back_populates="payments")
+class Pay(Base):
+    transaction_id: Mapped[int_pk]
+    amount = Mapped[decimal(18, 2)]
+    signature: Mapped[str]
+
+    account_id = Mapped[int] = mapped_column(ForeignKey("accounts.id"))
+
+    accounts = Mapped["Account"] = relationship("Account", back_populates="payments")
